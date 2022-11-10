5410f2c8ce0a98a28932ab19411985109b572a3c
Ali Asghar Chakera <70292482+princebiscuits@users.noreply.github.com>
2022-11-10 21:59:31 +0500

    done except plt recurring

--- Q1_kmeans.py
+++ Q1_kmeans.py
@@ -18,12 +18,14 @@ class KMeansClustering:
     def __init__(self, filename: str, K:int):
         self.image = mpimg.imread(filename)        
         self.K = K
-        self.height = self.image.shape[0]
-        self.width = self.image.shape[1]
+        self.height = self.image.shape[0] - 1
+        self.width = self.image.shape[1] - 1
         # print(self.width, self.height)
         self.centroids = list()
         self.clusters = dict()
+        self.index = dict()
         self.error = 9999
+        self.result = self.image.copy()
     
     
     def __generate_initial_centroids(self) -> list :
@@ -33,6 +35,7 @@ class KMeansClustering:
             # self.centroids.append((h, w, self.image[h][w]))
             self.centroids.append(self.image[h][w])
             self.clusters[i] = list()
+            self.index[i] = list()
         return self.centroids
     
     def __calculate_distance(self, p1: tuple, p2: tuple) -> float:
@@ -51,6 +54,7 @@ class KMeansClustering:
                         d, a, b, pos = dist, i, j, k
                 # self.clusters[pos].append((a, b, self.image[a][b]))
                 self.clusters[pos].append(self.image[a][b])
+                self.index[pos].append((a, b))
         return self.clusters
     
     
@@ -59,41 +63,54 @@ class KMeansClustering:
         for i in range(self.K):
             # rgb = self.clusters[i][2]
             rgb = self.clusters[i]
-            new_cluster = tuple(np.average(rgb, axis=0))
+            avg = np.average(rgb, axis=0)
+            new_cluster = (int(avg[0]), int(avg[1]), int(avg[2]))
+            # new_cluster = np.average(rgb, axis=0)
+            # print(new_cluster)
             # new = (new_cluster[0], new_cluster[1])
             self.error = self.__calculate_distance(self.centroids[i], new_cluster)
-            print(self.error)
+            # print(self.error)
             self.centroids[i] = new_cluster
-        return list()
+            self.clusters[i] = list()
+            self.index[i] = list()
+        return self.centroids
 
    
     def apply(self):
         #your code here to apply kmeans algorithm to cluster data loaded from the image file.
         self.__generate_initial_centroids()
+        self.__assign_clusters()
+        # self.show_result()
         while self.error > 0.5:
-            self.__assign_clusters()
             self.__recompute_centroids()
-        self.print_centroids()
+            self.__assign_clusters()
+        # self.show_result()
+        # self.print_centroids()
         self.__save_image()
         
-       
  
     def __save_image(self):
         #This function overwrites original image with segmented image to be shown later.
-        for i in self.clusters:
-            for j in self.clusters[i]:
-                self.clusters[i][j] = self.centroids[i]
-        
+        # self.result = self.image.copy()
+        for i in self.index:
+            count = 0
+            for j in self.index[i]:
+                # result[j[0]][j[1]] = j[2]
+                self.clusters[i][count] = self.centroids[i]
+                self.result[j[0]][j[1]] = self.clusters[i][count]
+                count += 1
+        # print(self.clusters[1][10])
     
     def show_result(self):
-        plt.imshow(self.image)
+        plt.imshow(self.result)
+        plt.show()
         
     def print_centroids(self):
         #This function prints all centroids formed by Kmeans clustering
         print(self.centroids)
        
         
-kmeans = KMeansClustering("images\sample1.jpg", 2)
+kmeans = KMeansClustering("images\sample1.jpg", 5)
 kmeans.apply()
 kmeans.show_result()
 # kmeans.print_centroids()    
